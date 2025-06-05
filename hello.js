e9f9009936188c3c29e2bc6507ee950ff2f332da
athrvaraut05 <rautathrva417@email.com>
2025-06-05 15:44:50 +0530

    done

--- hello.js
+++ hello.js
@@ -1,8 +1,31 @@
-let x=11
-if(x%2==0)
+// let x=11
+// if(x%2==0)
+// {
+// console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+// function sqaure(x)
+// {
+//  return x*x
+// }
+// console.log(sqaure(5))
+function yellow(name,callback,cd,cd)
 {
-console.log("even")
+    console.log(`colour ${name}`)
+    red()
 }
-else{
-    console.log("odd")
+function red(){
+    console.log("hiii")
+    green()
 }
+function green(){
+console.log("nothing")
+blue()
+}
+function blue(){
+    console.log("ggdg")
+   
+}
+yellow("athrva",red,green)
